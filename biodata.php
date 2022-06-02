a615fe9a96887d6c9ebcca44eb27c8db6f6ab011
aziz279 <73511815+aziz279@users.noreply.github.com>
2022-06-02 16:15:45 +0700

    Create biodata.php

---
+++ biodata.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>biodata</title>
+</head>
+<body>
+<?php
+echo"DATA DIRI
+<br>
+<br>
+<table width='700' height='200' border='0' >
+<tr>
+<td width='50'>&nbsp;</td>
+<td width='200' height='5'>NAMA LENGKAP</td>
+<td>:</td>
+<td width='500'> RIZKI ROMADHONI SANDY</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>NAMA PANGGILAN</td>
+<td>:</td>
+<td>SANDY</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> TTL</td>
+<td> :</td>
+<td>KEDIRI, 19 NOVEMBER 2001</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> JURUSAN</td>
+<td>:</td>
+<td>SISTEM INFORMASI </td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> ALAMAT</td>
+<td>:</td>
+<td>KAB.KEDIRI KEC.PURWOASRI PROV.JAWATIMUR </td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> Tinggi/Berat</td>
+<td>:</td>
+<td>180 cm/81 kg</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> STATUS</td>
+<td>:</td>
+<td>MAHASISWA</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> AGAMA</td>
+<td>:</td>
+<td>ISLAM</td>
+</tr>
+<tr>
+<td height='21'>&nbsp;</td>
+<td> HOBI</td>
+<td>:</td>
+<td>OTOMOTIF</td>
+</tr>
+</table>";
+?>   
+</body>
+</html>
